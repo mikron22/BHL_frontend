a67acc087d9684d02c4bea6d51b7266f5fb290a7
Michał Sakowicz <michalsakowicz99@gmail.com>
2021-04-10 11:16:41 +0200

    merged website

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,6 @@ import { ROUTE_FILEUPLOAD } from "../routeConstants"
 import { useState } from "react";
 
 
-
-
 const Home = () => {
     const [text, setText] = useState("Edit");
     const [mode, setMode] = useState("");
@@ -38,49 +36,49 @@ const Home = () => {
 
     return (
         <div className="container mt-4">
-            <div className="row">
-                <div className="jumbotron col-4">
-                    <h1><i className="fas fa-home"/> 24°C
-                    <br/>
+        <div className="row">
+            <div className="jumbotron col-4">
+                <h1><i className="fas fa-home"/> 24°C
+                <br/>
 
-                        <i className="fas fa-bolt pl-2"/> 24 kWh
-                    </h1>
-                </div>
-                <div className="jumbotron col-4">
-                    <p className="lead pt-2">
-                    <fieldset>
-                        <p> Tryb pracy: </p>
-
-                        <div class="form-group">
-                            <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input"  onClick={(e) => setMode("energooszczedny")} disabled = {text === "Edit"}/>
-                                <label class="custom-control-label" for="customRadio1"> energooszczędny </label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input" onClick={(e) => setMode("kompaktowy")} disabled = {text === "Edit"}/>
-                                <label class="custom-control-label" for="customRadio2"> kompaktowy </label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input" onClick={(e) => setMode("regeneracyjny")} disabled = {text === "Edit"}/>
-                                <label class="custom-control-label" for="customRadio3"> regeneracyjny </label>
-                            </div>
-                            <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio4" name="customRadio" class="custom-control-input" onClick={(e) => setMode("energochlonny")} disabled = {text === "Edit"}/>
-                                <label class="custom-control-label" for="customRadio4"> energochłonny </label>
-                            </div>
-                        </div>
-                        </fieldset>
-                        </p>
-                    <button type="button" class="btn btn-primary" onClick={(e) => handleClick(e)}>{text}</button>
-                </div>
-                <div className="jumbotron col-4">
-                    <h1>
-                        <i className="fas fa-home"/> 24°C
-                        <img id="XD" src="http://cdn.weatherapi.com/weather/64x64/night/113.png?fbclid=IwAR1Huegx9wSvHF9HXqfkfaKVGY06ofn6MRjd429oJFlW-SzwHq2_p5PlBGg"/>
-                        <button onClick={() => weather(dispatch, data)}>abc</button>
-                    </h1>
-                </div>
+                    <i className="fas fa-bolt pl-2"/> 24 kWh
+                </h1>
             </div>
+            <div className="jumbotron col-4">
+                <p className="lead pt-2">
+                <fieldset>
+                    <p> Tryb pracy: </p>
+
+                    <div class="form-group">
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input"  onClick={(e) => setMode("energooszczedny")} disabled = {text === "Edit"}/>
+                            <label class="custom-control-label" for="customRadio1"> energooszczędny </label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input" onClick={(e) => setMode("kompaktowy")} disabled = {text === "Edit"}/>
+                            <label class="custom-control-label" for="customRadio2"> kompaktowy </label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input" onClick={(e) => setMode("regeneracyjny")} disabled = {text === "Edit"}/>
+                            <label class="custom-control-label" for="customRadio3"> regeneracyjny </label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio4" name="customRadio" class="custom-control-input" onClick={(e) => setMode("energochlonny")} disabled = {text === "Edit"}/>
+                            <label class="custom-control-label" for="customRadio4"> energochłonny </label>
+                        </div>
+                    </div>
+                    </fieldset>
+                    </p>
+                <button type="button" class="btn btn-primary" onClick={(e) => handleClick(e)}>{text}</button>
+            </div>
+            <div className="jumbotron col-4">
+                <h1>
+                    <i className="fas fa-home"/> 24°C
+                    <img id="XD" src="http://cdn.weatherapi.com/weather/64x64/night/113.png?fbclid=IwAR1Huegx9wSvHF9HXqfkfaKVGY06ofn6MRjd429oJFlW-SzwHq2_p5PlBGg"/>
+                    <button onClick={() => weather(dispatch, data)}>abc</button>
+                </h1>
+            </div>
+        </div>
         </div>
     )}
 
